5883928cae12ba128095983ab9d39c78251ab4f7
JasmeerSangha <61358666+JasmeerSangha@users.noreply.github.com>
2020-03-12 09:22:54 -0400

    Added election audit code.

--- Pypoll.py
+++ Pypoll.py
@@ -1,30 +1,73 @@
-
+# Add our dependencies.
 import csv
 import os
-electfile = os.path.join('Resources','election_results.csv')
-outerfile=os.path.join('analysis','election_analysis.txt')
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources/election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-with open(electfile) as election_data:
-    
+total_votes=0
+candidateoptions=[]
+candidatevotes={}
+winning_votes=0
+winning_cand=""
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        candidatename=row[2]
+        if candidatename not in candidateoptions:
+            
+            candidateoptions.append(candidatename)
+            candidatevotes[candidatename]=0
+        candidatevotes[candidatename]+=1
+        total_votes+=1
+
+for name in candidateoptions:
+    if candidatevotes[name] > winning_votes:
+        winning_cand=name
+        winning_votes=candidatevotes[name]
+        
+    print(f'{name} got {candidatevotes[name]} ({100*candidatevotes[name]/total_votes:,.2f}%) votes.')
+print(
+    f"-------------------------\n"
+    f"Winner: {winning_cand}\n"
+    f"Winning Vote Count: {winning_votes:,}\n"
+    f"Winning Percentage: {100*winning_votes/total_votes:.1f}%\n"
+    f"-------------------------\n")
+
+ 
     
     
     
-     
     
     
-with open(outerfile,'w') as txtfile:
+with open(file_to_save,'w') as txtfile:
     
     txtfile.write("Counties in Election\n----------------------\na\nb\nc")
-#outfile.close()
 
+
+
+#voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+#{"county":"Denver", "registered_voters": 463353},
+#{"county":"Jefferson", "registered_voters": 432438}]
+##
+#for i in voting_data:
+#    
+#    message=(f"{i['county']} county has {i['registered_voters']} registered voters.")
+#    print(message)
     
-
-
-
+    
+    
+#my_votes = int(input("How many votes did you get in the election? "))
+#total_votes = int(input("What is the total votes in the election? "))
+#print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+    
 
 
 
